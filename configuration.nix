452595759e28def519c68232b1b16f3edf0a9155
RealLanta <95084339+RealLanta@users.noreply.github.com>
2022-05-07 17:36:11 +0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -39,7 +39,7 @@
   programs.steam.enable = true;
   
   # Realtek Wireless Drive
-  boot.extraModulePackages = [ config.boot.kernelPackages.rtl8811cu ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.rtl8821cu ];
 
   # Use the grub EFI boot loader
   #boot.loader = {
